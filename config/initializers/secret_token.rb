bcd68ad0f78166ab50679caa65d50bb49ba59845
Jon Baldivieso <jbaldivieso@gmail.com>
2013-06-01 12:35:52 -0700

    Default output from rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Birdvsbug::Application.config.secret_token = 'ea38643acab09bb2a7b4ffaa354e7b96af317822cc99e23b20f6c710fb24d5a583624010c3beba9a963cb5186ee1e5f6f5f0c7f4a805de2051f1925f4f4717cc'
